0f99b67e55740cdab026b598c9fa7f8544b0fbae
Hem Raj Regmi <sangamsyabil@gmail.com>
2024-12-04 11:11:51 -0500

    flake8 implemented

--- New Dashboard/app.py
+++ New Dashboard/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 dashboard_app = Flask(__name__)
 
+
 @dashboard_app.route('/')
 def dashboard():
-    
+
     return render_template(
         'dashboard.html',
         title="<name> Dashboard",
         name="<name> Dashboard")
+
 
 if __name__ == "__main__":
     dashboard_app.run(host='0.0.0.0', port=8080, debug=True)
