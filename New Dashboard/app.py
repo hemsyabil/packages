532b8a7d0ec4555b55ebab1c73a7393b52ee4575
Hem Raj Regmi <sangamsyabil@gmail.com>
2024-12-05 11:14:53 -0500

    basic setup done V0

--- New Dashboard/app.py
+++ New Dashboard/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template
 
-dashboard_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@dashboard_app.route('/')
+@app.route('/')
 def dashboard():
-
     return render_template(
         'dashboard.html',
-        title="<name> Dashboard",
-        name="<name> Dashboard")
-
+        title="TCM Dashboard",
+        name="TCM Dashboard")
 
 if __name__ == "__main__":
-    dashboard_app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
